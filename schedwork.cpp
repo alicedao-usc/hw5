71cf092af525f97bbb82ef04defdcf2eb0a6d37b
Alice Dao <alicedao@usc.edu>
2022-12-04 07:29:50 +0000

    Schedwork

--- schedwork.cpp
+++ schedwork.cpp
@@ -1,5 +1,3 @@
-
-
 #include <set>
 #include <iostream>
 #include <fstream>
@@ -19,7 +17,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+void schedFill(const AvailabilityMatrix& avail, const size_t dailyNeed, DailySchedule& sched);
+bool isValid(const size_t maxShifts, const int worker, DailySchedule& sched);
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int day,
+    int col
+    );
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,12 +44,82 @@ bool schedule(
 
 
     //recursive solution is based around dates
-    //for each date try all of the combinations of the employees that are free so it satisfies working employee
-    //subtract number of days that the worker can work for maximum maxShifts
-    //keep track of 2d matrix for assigning
+    //if all days are filled up then we know a valid solution can be made
+    //we will recurse through each day and for each day we will find the workers that are available and 
+    //slot them in
+    //if our assignment is not valid, then we can just back track
 
-    
 
+    schedFill(avail, dailyNeed, sched);
+    bool valid = scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0);
+    return valid;
+}
+
+void schedFill(const AvailabilityMatrix& avail, const size_t dailyNeed, DailySchedule& sched){
+    for(unsigned int i = 0; i < avail.size(); i++){
+        vector<Worker_T> t;
+        for(unsigned int j = 0; j < dailyNeed; j++){
+            t.push_back(INVALID_ID);
+        }
+        sched.push_back(t);
+    }
+}
+
+
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    int day,
+    int col)
+    {
+
+    if((unsigned int)day == avail.size()){
+        return true;
+    }
+
+    if((unsigned int)col == dailyNeed){
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, day + 1, 0);
+
+    }
+    else if(sched[day][col] == INVALID_ID){
+        for(unsigned int i=0; i< avail[day].size(); i++){
+            if(avail[day][i] == 1){
+                sched[day][col] = i;
+                if(isValid(maxShifts, i, sched)){
+                    if(scheduleHelper(avail, dailyNeed, maxShifts, sched, day, col + 1)){
+                        return true;
+                    }
+                }
+            }
+        }
+        sched[day][col] = INVALID_ID;
+    }
+
+    return false;
 
 }
 
+
+bool isValid(const size_t maxShifts, const int worker, DailySchedule& sched){
+    
+    unsigned int dailyS = 0;
+    unsigned int weeklyS = 0;
+
+    for(unsigned int i = 0; i < sched.size(); i++){
+        for(unsigned int j = 0; j < sched[i].size(); j++){
+            if(sched[i][j] == (unsigned int)worker){
+                dailyS++;
+                weeklyS++;
+            }
+        }
+        if(dailyS > 1){
+            return false;
+        }
+        dailyS = 0;
+    }
+    if(weeklyS > maxShifts){
+        return false;
+    }
+    return true;
+}
